293037a17df113458a15bc20774a6574c0437a18
wjseoduq3 <dragon65@nate.com>
2024-06-18 11:26:19 +0900

    1'st commit

---
+++ src/main/java/com/jdy/jdbctest/JdbcController.java
@@ -0,0 +1,43 @@
+package com.jdy.jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JdbcController {
+	
+	@RequestMapping(value = "/test")
+	public void test() {
+		
+		String driverName = "com.mysql.jdbc.Driver";
+		String url ="jdbc:mysql://localhost:3306/jdy_spring_projectdb";
+		String username ="root";
+		String password ="12345";
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName(driverName);
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println(conn);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+					}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
